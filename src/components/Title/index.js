3bce1aaa8da37e3d309e850df7be316bcba64a04
ashkan <ashkan.ram70@gmail.com>
2019-10-25 21:16:19 -0700

    added some style into the app

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,11 +4,11 @@ import "./style.css";
 function Title(props) {
   return (
     <div className="row">
-      <div className="scores">
-        Score: {props.score} Highscore: {props.highscore}
-      </div>
-      <h3 className="title">{props.children}</h3>
       
+      <h3 className="title">{props.children}</h3>
+      <div className="scores text-center col-md-12">
+        <span className="scoreClass">Score:</span> {props.score} <span className="highScoreClass">Highscore: </span>{props.highscore}
+      </div>
     </div>
   )
 }
